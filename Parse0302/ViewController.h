bbe1b682f6d0a5af60834d36f42567593cd98be5
timothy-20 <jungun3329@gmail.com>
2021-03-02 11:31:07 +0900

    Initial Commit

---
+++ Parse0302/ViewController.h
@@ -0,0 +1,14 @@
+//
+//  ViewController.h
+//  Parse0302
+//
+//  Created by 임정운 on 2021/03/02.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Parse0302/ViewController.m
@@ -0,0 +1,22 @@
+//
+//  ViewController.m
+//  Parse0302
+//
+//  Created by 임정운 on 2021/03/02.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+
+@end
